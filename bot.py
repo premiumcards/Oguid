fa6ae2124a8041da6f5562ca6516f0c36b90e2d6
premiumcards <rayomensukuna0001@gmail.com>
2026-09-17 08:51:16 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -5,8 +5,14 @@ from http.server import HTTPServer, BaseHTTPRequestHandler
 class Handler(BaseHTTPRequestHandler):
     def do_GET(self):
         self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
         self.end_headers()
         self.wfile.write(b"Bot is alive!")
+
+    def do_HEAD(self):
+        self.send_response(200)
+        self.send_header('Content-Type', 'text/plain')
+        self.end_headers()
 
     
 
