97bc789025e7d391c44009d1089a716b730820cb
premiumcards <rayomensukuna0001@gmail.com>
2026-09-17 08:59:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,11 @@ class Handler(BaseHTTPRequestHandler):
         self.send_header('Content-Type', 'text/plain')
         self.end_headers()
 
-    
+    def run_web():
+    port = int(os.environ.get("PORT", 8080))
+    HTTPServer(("0.0.0.0", port), Handler).serve_forever()
+
+Thread(target=run_web, daemon=True).start()
 
 import os
 import sys
